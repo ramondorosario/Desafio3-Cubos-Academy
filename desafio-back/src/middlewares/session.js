a787b08194bb77395eb26563859d897f340e7725
Ramon do Rosário <ramondorosario@gmail.com>
2020-10-21 19:45:15 -0300

    Funcionalidade: tratamento de erro quando não for informado um token

--- desafio-back/src/middlewares/session.js
+++ desafio-back/src/middlewares/session.js
@@ -4,6 +4,8 @@ const response = require('../utils/response');
 require('dotenv').config();
 
 const verify = async (ctx, next) => {
+	if (!ctx.headers.authorization)
+		return response(ctx, 403, 'usuário não autenticado');
 	const token = ctx.headers.authorization.split(' ')[1];
 	try {
 		const verification = await jwt.verify(token, process.env.JWT_SECRET);
