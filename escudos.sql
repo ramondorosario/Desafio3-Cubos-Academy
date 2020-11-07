4594a9fd3168e64df43bacd3cd4d89ab0cd9b72b
Ramon do Rosário <ramondorosario@gmail.com>
2020-11-07 11:23:05 -0300

    Arquivo tabela escudos

---
+++ escudos.sql
@@ -0,0 +1,25 @@
+create table escudos (
+	time TEXT,
+	url TEXT
+);
+
+insert into escudos ( time, url) values ('Flamengo', 'https://ssl.gstatic.com/onebox/media/sports/logos/orE554NToSkH6nuwofe7Yg_48x48.png');
+insert into escudos ( time, url) values ('Santos', 'https://ssl.gstatic.com/onebox/media/sports/logos/VHdNOT6wWOw_vJ38GMjMzg_48x48.png');
+insert into escudos ( time, url) values ('Palmeiras', 'https://ssl.gstatic.com/onebox/media/sports/logos/7spurne-xDt2p6C0imYYNA_48x48.png');
+insert into escudos ( time, url) values ('Grêmio', 'https://ssl.gstatic.com/onebox/media/sports/logos/Ku-73v_TW9kpex-IEGb0ZA_48x48.png');
+insert into escudos ( time, url) values ('Athletico-PR', 'https://ssl.gstatic.com/onebox/media/sports/logos/LtpA9v-CQ_Qs2bgWVdDXxQ_48x48.png');
+insert into escudos ( time, url) values ('São Paulo', 'https://ssl.gstatic.com/onebox/media/sports/logos/4w2Z97Hf9CSOqICK3a8AxQ_48x48.png');
+insert into escudos ( time, url) values ('Inter-RS', 'https://ssl.gstatic.com/onebox/media/sports/logos/OWVFKuHrQuf4q2Wk0hEmSA_48x48.png');
+insert into escudos ( time, url) values ('Corinthians', 'https://ssl.gstatic.com/onebox/media/sports/logos/tCMSqgXVHROpdCpQhzTo1g_48x48.png');
+insert into escudos ( time, url) values ('Fortaleza', 'https://ssl.gstatic.com/onebox/media/sports/logos/me10ephzRxdj45zVq1Risg_48x48.png');
+insert into escudos ( time, url) values ('Goiás', 'https://ssl.gstatic.com/onebox/media/sports/logos/gb8bo2x00XsbvsVp9nGniA_48x48.png');
+insert into escudos ( time, url) values ('Bahia', 'https://ssl.gstatic.com/onebox/media/sports/logos/nIdbR6qIUDyZUBO9vojSPw_48x48.png');
+insert into escudos ( time, url) values ('Vasco', 'https://ssl.gstatic.com/onebox/media/sports/logos/_4RUqwd5euAxf0MtbPwq4A_48x48.png');
+insert into escudos ( time, url) values ('Atlético-MG', 'https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_48x48.png');
+insert into escudos ( time, url) values ('Fluminense', 'https://ssl.gstatic.com/onebox/media/sports/logos/fCMxMMDF2AZPU7LzYKSlig_48x48.png');
+insert into escudos ( time, url) values ('Botafogo', 'https://ssl.gstatic.com/onebox/media/sports/logos/KLDWYp-H8CAOT9H_JgizRg_48x48.png');
+insert into escudos ( time, url) values ('Ceará', 'https://ssl.gstatic.com/onebox/media/sports/logos/mSl0cz3i2t8uv4zcprobOg_48x48.png');
+insert into escudos ( time, url) values ('Cruzeiro', 'https://ssl.gstatic.com/onebox/media/sports/logos/cfkLbPGt7TD_FSDotajcbA_48x48.png');
+insert into escudos ( time, url) values ('CSA', 'https://ssl.gstatic.com/onebox/media/sports/logos/xQkyuz2y935JX0uIi9w6Lg_48x48.png');
+insert into escudos ( time, url) values ('Chapecoense', 'https://ssl.gstatic.com/onebox/media/sports/logos/cZ4ga5Fdqe3Pd-dEcpjUmg_48x48.png');
+insert into escudos ( time, url) values ('Avaí', 'https://ssl.gstatic.com/onebox/media/sports/logos/9cwCmoBXGaPJ_Q5cgUeocg_48x48.png');
